4bbac4bb1312c7a52ac95932b90f33a111ce8482
Steve Glardon <sglardon@gmail.com>
2011-12-25 14:07:39 +0100

    project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voicebox::Application.config.secret_token = '20dfe13dff9bdccbb1554ad454f334c8edb46d0430217f1cc9078216487931985734d48546d800993bc61a8fa2f6e043f3e6fd582e217bdb8e8d80278928d8f0'
